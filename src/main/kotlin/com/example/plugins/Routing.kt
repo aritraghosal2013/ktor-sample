8a6846b681611f71f51113778e5f5b733642e332
aag <aritraghosal2013@gmail.com>
2022-12-27 10:07:25 +0530

    Path Params and Query Params added

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -2,6 +2,7 @@ package com.example.plugins
 
 import io.ktor.server.routing.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -10,7 +11,16 @@ fun Application.configureRouting() {
 
     routing {
         get("/") {
+            println("Request Headers are ${call.request.headers.names()}")
+            println("Query Parameters are ${call.request.queryParameters.names()}")
+            println("Path Parameters are ${call.request.path()}")
             call.respondText("Hello World!")
+        }
+
+        get("/user/{userId}") {
+            println("Path Parameters are ${call.parameters.names()}")
+            println("Value of Path parameter userId is: ${call.parameters["userId"]}")
+            call.respondText("User Name is Alex")
         }
     }
 }
